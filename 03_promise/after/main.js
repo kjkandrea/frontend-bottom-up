ff3d8b9bce9b3df2b36b9bbe3dbce69f664fb48f
jongkeun-kim <kjkandrea@gmail.com>
2022-04-30 15:03:54 +0900

    Refactor after : promise subject

--- 03_promise/after/main.js
+++ 03_promise/after/main.js
@@ -1,14 +1,17 @@
 const $ipAddress = document.getElementById('ip-address')
 
-const fetchMyIp = () => fetch('https://api64.ipify.org?format=json').then(json => json.json())
+const fetchMyIp = () =>
+  fetch('https://api64.ipify.org?format=json')
+    .then(json => json.json())
+    .catch(console.error)
 
 const renderIpAddress = ipAddressText => {
   $ipAddress.innerText = ipAddressText
 }
 
-const main = () =>
-  fetchMyIp()
-    .then(({ ip }) => renderIpAddress(ip))
-    .catch(console.error)
+const main = async () => {
+  const { ip } = await fetchMyIp()
+  renderIpAddress(ip)
+}
 
 main()
