263ff7ba1c895d8b28df36a974a030a45a5ad6c0
jongkeun-kim <kjkandrea@gmail.com>
2022-04-30 14:53:33 +0900

    Add before : promise subject

---
+++ 03_promise/before/main.js
@@ -0,0 +1,23 @@
+(function () {
+  var $ipAddress = document.getElementById('ip-address')
+
+  function getMyIp (callback) {
+    $.ajax({
+      type: 'GET',
+      url: 'https://api64.ipify.org?format=json',
+      contentType : "application/json; charset=utf-8",
+      success: function (response) {
+        callback(response.ip)
+      },
+      error: function (error) {
+        console.error(error)
+      }
+    })
+  }
+
+  function renderIpAddress (ipAddressText) {
+    $ipAddress.innerText = ipAddressText
+  }
+
+  getMyIp(renderIpAddress)
+}())
