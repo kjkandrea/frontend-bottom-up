9593f205a691f19187fb2ba7ab3f6a1e3ef141a5
jongkeun-kim <kjkandrea@gmail.com>
2022-04-30 15:46:10 +0900

    Add before : framework subject

---
+++ 04_framework/before/main.js
@@ -0,0 +1,56 @@
+import todos from '../mock'
+
+(function () {
+  var $app = document.getElementById('app')
+
+  function main() {
+    renderTodo(todos);
+  }
+
+  function renderTodo(todos) {
+    $app.append(getTodosElement(todos))
+  }
+
+  function getTodosElement (todos) {
+    var $todoList = document.createElement('ul')
+
+    for (var i = 0; i < todos.length ;i += 1) {
+      var $todoItem = document.createElement('li')
+      $todoItem.id = getTodoId(todos[i].id)
+      $todoItem.append(getTodoElement(todos[i]))
+      $todoList.append($todoItem)
+    }
+
+    return $todoList;
+  }
+
+  function getTodoElement (todo) {
+    var $todo = document.createElement('div')
+    var $paragraph = document.createElement('p')
+    var $deleteButton = document.createElement('button')
+    var contentTextNode = document.createTextNode(todo.content)
+    var deleteButtonTextNode = document.createTextNode('완료')
+    $deleteButton.addEventListener('click', function () {
+      deleteTodoById(todo.id)
+    })
+
+    $paragraph.append(contentTextNode)
+    $deleteButton.append(deleteButtonTextNode)
+    $todo.append($paragraph)
+    $todo.append($deleteButton)
+
+    return $todo;
+  }
+
+  function getTodoId (id) {
+    var todoIdNamespace = 'todo-item-'
+
+    return todoIdNamespace + id
+  }
+
+  function deleteTodoById (id) {
+    document.getElementById(getTodoId(id)).remove()
+  }
+
+  main()
+}())
---
+++ 04_framework/mock.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    content: '일하기'
+  },
+  {
+    id: 2,
+    content: '놀기'
+  },
+  {
+    id: 3,
+    content: '먹기'
+  },
+  {
+    id: 4,
+    content: '공부하기'
+  },
+]
